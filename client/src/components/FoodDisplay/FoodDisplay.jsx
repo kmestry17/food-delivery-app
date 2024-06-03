6819c40786c9750a02491e930639fef56d6bda5b
Kunal Mestry <kmestry17@gmail.com>
2024-06-02 19:56:20 -0500

    Categorize and filter food items

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,20 +10,22 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list ? (
-          food_list.map((item, index) => (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          ))
-        ) : (
-          <p>Loading food list...</p>
-        )}
+        {food_list.map((item, index) => {
+          console.log(category, item.category);
+
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
